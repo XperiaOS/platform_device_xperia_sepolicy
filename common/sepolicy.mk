96b4879ce7d010168c6b827774b707c26581a5ca
Ethan Chen <intervigil@gmail.com>
2017-10-18 19:11:22 -0700

    sepolicy: Move fiOS policy to platform
    Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,5 +3,9 @@
 # inherit from fiOS
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/fi/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/fi/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/fi/sepolicy/common/private
+
